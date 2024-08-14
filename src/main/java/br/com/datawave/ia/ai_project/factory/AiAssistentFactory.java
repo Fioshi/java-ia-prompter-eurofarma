dd9a84c338b095a4e7c7b3b7d3e911162ad287c2
Fioshi <guilhermefiochi@outlook.com>
2024-08-14 11:43:31 -0300

    feat: alterado LLM

--- src/main/java/br/com/datawave/ia/ai_project/factory/AiAssistentFactory.java
+++ src/main/java/br/com/datawave/ia/ai_project/factory/AiAssistentFactory.java
@@ -1,8 +1,10 @@
 package br.com.datawave.ia.ai_project.factory;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
 
+import javax.sql.DataSource;
 import java.time.Duration;
 
 public class AiAssistentFactory {
@@ -10,7 +12,7 @@ public class AiAssistentFactory {
     public static ChatLanguageModel  createHuggingFace(String token){
         return HuggingFaceChatModel.builder()
                 .accessToken(token)
-                .modelId("google/gemma-2-2b-it")
+                .modelId("TheBloke/Wizard-Vicuna-13B-Uncensored-GGUF")
                 .timeout(Duration.ofSeconds(300))
                 .build();
     }
