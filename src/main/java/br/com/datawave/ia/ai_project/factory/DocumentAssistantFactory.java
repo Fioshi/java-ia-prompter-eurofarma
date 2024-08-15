3668ff8b7ffd2211d7fbd5b5c8a03c87d2ef0a6b
Fioshi <guilhermefiochi@outlook.com>
2024-08-15 16:56:45 -0300

    feat: criado assistente do factory

---
+++ src/main/java/br/com/datawave/ia/ai_project/factory/DocumentAssistantFactory.java
@@ -0,0 +1,27 @@
+package br.com.datawave.ia.ai_project.factory;
+
+import br.com.datawave.ia.ai_project.utils.DocumentService;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.service.AiServices;
+
+public class DocumentAssistantFactory {
+    private DocumentService documentAiService;
+
+    public DocumentAssistantFactory(ChatLanguageModel chatModel, ContentRetriever contentRetriever) {
+        documentAiService = buildDocumentAiService(chatModel, contentRetriever);
+    }
+
+    public String chat(String message) {
+        return documentAiService.chat(message);
+    }
+
+    private DocumentService buildDocumentAiService(ChatLanguageModel chatModel, ContentRetriever contentRetriever) {
+        return AiServices.builder(DocumentService.class)
+                .chatLanguageModel(chatModel)
+                .contentRetriever(contentRetriever)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+    }
+}
